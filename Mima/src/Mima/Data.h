76cc2a8a156bb27a8bd4a95ce99742f252354192
OrtwIn <bertil.braun.private@gmail.com>
2020-12-22 19:57:56 +0100

    Compiler settings added

--- Mima/src/Mima/Data.h
+++ Mima/src/Mima/Data.h
@@ -1,15 +1,19 @@
 #pragma once
 
+#pragma pack(push, 1)
 // Wrapper for a 24bit signed integer
 struct value {
     value() : value(0) {}
     value(int v) : v(v) {}
     int v : 24;
 };
+#pragma pack(pop)
 
+#pragma pack(push, 1)
 // Wrapper for a 20bit signed integer
 struct addr {
     addr() : addr(0) {}
     addr(int v) : v(v) {}
     unsigned int v : 20;
 };
+#pragma pack(pop)
